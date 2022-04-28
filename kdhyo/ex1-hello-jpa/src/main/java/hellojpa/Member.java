5a88f8aa6bc6f4fc7c0b3163d30b51f58856de95
dongHyo <kdhyo98@gmail.com>
2022-04-28 15:51:13 +0900

    feat: 타입 테스트

--- kdhyo/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ kdhyo/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,7 +1,13 @@
 package hellojpa;
 
+import java.util.Date;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
 public class Member {
@@ -9,7 +15,19 @@ public class Member {
 	@Id
 	private Long id;
 
+	@Column(name = "username")
 	private String name;
+
+	private Integer age;
+
+	@Enumerated(EnumType.STRING)
+	private RoleType roleType;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date createdDate;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date lastModifyDate;
 
 	public Long getId() {
 		return id;
