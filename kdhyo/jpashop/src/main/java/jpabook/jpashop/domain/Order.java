2f2e42eb584f928d6cfcda31f3fedfae3e8cce8f
dongHyo <kdhyo98@gmail.com>
2022-04-29 22:44:49 +0900

    feat : 기본매핑

---
+++ kdhyo/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,58 @@
+package jpabook.jpashop.domain;
+
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.print.attribute.standard.MediaSize.NA;
+
+@Entity
+@Table(name = "ORDERS")
+public class Order {
+
+	@Id @GeneratedValue
+	@Column(name = "ORDER_ID")
+	private Long id;
+	@Column(name = "MEMBER_ID")
+	private Long memberId;
+	private LocalDateTime orderDate;
+
+	@Enumerated(EnumType.STRING)
+	private OrderStatus status;
+
+	public Long getId() {
+		return id;
+	}
+
+	public Long getMemberId() {
+		return memberId;
+	}
+
+	public LocalDateTime getOrderDate() {
+		return orderDate;
+	}
+
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setMemberId(Long memberId) {
+		this.memberId = memberId;
+	}
+
+	public void setOrderDate(LocalDateTime orderDate) {
+		this.orderDate = orderDate;
+	}
+
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+}
